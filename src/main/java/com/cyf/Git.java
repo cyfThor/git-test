7f9d6b2b4d85e3c484d90ccab5d1833d9cf0535b
cyfThor <107460391+cyfThor@users.noreply.github.com>
2022-07-07 12:37:26 +0800

    Update Git.java

--- src/main/java/com/cyf/Git.java
+++ src/main/java/com/cyf/Git.java
@@ -8,5 +8,6 @@ public class Git {
         System.out.println("Hello Git4");
         System.out.println("Hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
