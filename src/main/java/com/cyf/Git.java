34be0f117273bd98767e83d85509368a40871fe8
cyfThor <107460391+cyfThor@users.noreply.github.com>
2022-07-07 13:08:10 +0800

    pull test

--- src/main/java/com/cyf/Git.java
+++ src/main/java/com/cyf/Git.java
@@ -10,5 +10,6 @@ public class Git {
         System.out.println("push test");
         System.out.println("push test1");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
 }
